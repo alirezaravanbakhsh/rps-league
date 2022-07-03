f50b9238ee383da88c8b215548e1c84e712bacaf
Behrang Norouzinia <behrangn@gmail.com>
2022-07-03 15:43:11 +0430

    Add payment channel game of Rock-Paper-Scissors

---
+++ public/client.js
@@ -0,0 +1,471 @@
+function findOne(q) {
+  return document.querySelector(q)
+}
+
+function hide(el) {
+  el.style.display = 'none'
+}
+
+function hideAll() {
+  const divEls = document.querySelectorAll('main>div')
+  divEls.forEach(function (divEl) {
+    if (divEl.id !== 'errorView' && divEl.id !== 'channelBalanceView') {
+      hide(divEl)
+    }
+  })
+}
+
+function show(el) {
+  hideAll()
+  el.style.display = ''
+  const input = el.querySelector('input')
+  if (input != null) {
+    input.focus()
+  }
+}
+
+function showError(msg) {
+  errorViewEl.innerText = msg
+}
+
+function hideError() {
+  errorViewEl.innerText = ' '
+}
+
+function timeout(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, ms)
+  })
+}
+
+const channelBalanceViewEl = findOne('#channelBalanceView')
+const channelBalanceEl = findOne('#channelBalance')
+
+const errorViewEl = findOne('#errorView')
+
+const walletViewEl = findOne('#walletView')
+
+const balanceViewEl = findOne('#balanceView')
+const balanceFieldEl = findOne('#balanceField')
+const balanceAddressEl = findOne('#balanceAddress')
+const balanceCreateChannelEl = findOne('#balanceCreateChannel')
+
+const createChannelViewEl = findOne('#createChannelView')
+
+const initChannelViewEl = findOne('#initChannelView')
+
+const connectingViewEl = findOne('#connectingView')
+
+const hallViewEl = findOne('#hallView')
+const hallJoinEl = findOne('#hallJoin')
+
+const waitViewEl = findOne('#waitView')
+const waitLeaveEl = findOne('#waitLeave')
+
+const gameViewEl = findOne('#gameView')
+const gameRoomEl = findOne('#gameRoom')
+const gameLeaveEl = findOne('#gameLeave')
+const gamePickREl = findOne('#gamePickR')
+const gamePickPEl = findOne('#gamePickP')
+const gamePickSEl = findOne('#gamePickS')
+const gameYourPickEl = findOne('#gameYourPick')
+const gameTheirPickEl = findOne('#gameTheirPick')
+const gameDrawEl = findOne('#gameDraw')
+const gameWonEl = findOne('#gameWon')
+const gameLostEl = findOne('#gameLost')
+
+const state = {
+  wsServer: null,
+  channelId: null,
+  seed: null,
+  keyPair: null,
+  wallet: null,
+  publicKeyA: null,
+  addressA: null,
+  addressB: null,
+  balance: null,
+  channelB: null,
+  fromWalletB: null,
+  channelState: null
+}
+
+hideAll()
+hide(channelBalanceViewEl)
+setupWebSocket()
+
+// TonWeb
+const TonWeb = window.TonWeb
+const BN = TonWeb.utils.BN
+const toNano = TonWeb.utils.toNano
+const fromNano = TonWeb.utils.fromNano
+const providerUrl = 'https://testnet.toncenter.com/api/v2/jsonRPC'
+const apiKey = '0b673465b3dff8f572d26adb553f2bdabcd894c19d7b74c8104d6b3fb56b00bc'
+const tonweb = new TonWeb(new TonWeb.HttpProvider(providerUrl, {apiKey}))
+
+async function setupWallet(addressA, publicKeyA) {
+  show(walletViewEl)
+  const channelId = localStorage.getItem('channelId') || '1'
+  state.channelId = new BN(channelId)
+  const seedBase64 = localStorage.getItem('seed')
+  let seed = null
+  if (seedBase64) {
+    seed = TonWeb.utils.base64ToBytes(seedBase64)
+  } else {
+    seed = TonWeb.utils.newSeed()
+    localStorage.setItem('seed', TonWeb.utils.bytesToBase64(seed))
+  }
+  state.seed = seed
+  state.keyPair = TonWeb.utils.keyPairFromSeed(seed)
+  state.wallet = tonweb.wallet.create({publicKey: state.keyPair.publicKey, wc: 0})
+  state.publicKeyA = TonWeb.utils.base64ToBytes(publicKeyA)
+  state.addressA = new TonWeb.utils.Address(addressA)
+  state.addressB = await state.wallet.getAddress()
+  await checkBalance()
+}
+
+async function checkBalance() {
+  balanceFieldEl.innerText = '...'
+  balanceAddressEl.innerText = state.addressB.toString(true, true, true)
+  show(balanceViewEl)
+  balance = await tonweb.getBalance(state.addressB)
+  state.balance = new BN(balance)
+  balanceFieldEl.innerText = fromNano(state.balance)
+}
+
+balanceCreateChannelEl.addEventListener('click', function() {
+  hideError()
+  tonweb.getBalance(state.addressB).then(function(balance) {
+    state.balance = new BN(balance)
+    balanceFieldEl.innerText = fromNano(state.balance)
+    if (state.balance >= toNano('1')) {
+      createChannel()
+    } else {
+      showError('Please first deposit at least 1 TON to your wallet on testnet.')
+    }
+  })
+})
+
+async function createChannel() {
+  show(createChannelViewEl)
+  const channelInitState = {
+    balanceA: toNano('1'),
+    balanceB: toNano('1'),
+    seqnoA: new BN(0),
+    seqnoB: new BN(0)
+  }
+  state.channelState = channelInitState
+  const channelConfig = {
+    channelId: state.channelId,
+    addressA: state.addressA,
+    addressB: state.addressB,
+    initBalanceA: channelInitState.balanceA,
+    initBalanceB: channelInitState.balanceB
+  }
+  state.channelB = tonweb.payments.createChannel({
+    ...channelConfig,
+    isA: false,
+    myKeyPair: state.keyPair,
+    hisPublicKey: state.publicKeyA
+  })
+  state.fromWalletB = state.channelB.fromWallet({
+    wallet: state.wallet,
+    secretKey: state.keyPair.secretKey
+  })
+  const channelAddress = await state.channelB.getAddress()
+  console.log('channel address: %s', channelAddress.toString(true, true, true))
+  try {
+    const channelState = await state.channelB.getChannelState()
+    console.log('channel state: %s', channelState)
+  } catch (e) {
+    await state.fromWalletB.deploy().send(toNano('0.05'))
+    let i = 30
+    while (i > 0) {
+      i -= 1
+      try {
+        const channelState = await state.channelB.getChannelState()
+        console.log('channel state: %s', channelState)
+        break
+      } catch (e) {
+      }
+      await timeout(1000)
+    }
+  }
+  const data = await state.channelB.getData()
+  console.log(data)
+  console.log('balanceA: %s', data.balanceA.toString())
+  console.log('balanceB: %s', data.balanceB.toString())
+  if (data.balanceB.toString() === '0') {
+    await state.fromWalletB
+      .topUp({coinsA: new BN(0), coinsB: channelInitState.balanceB})
+      .send(channelInitState.balanceB.add(toNano('0.05')))
+    let i = 30
+    while (i > 0) {
+      i -= 1
+      try {
+        const data = await state.channelB.getData()
+        console.log('balanceB: %s', data.balanceB.toString())
+        if (data.balanceB.toString() !== '0') {
+          break
+        }
+      } catch (e) {
+      }
+      await timeout(1000)
+    }
+    }
+  state.wsServer.send(JSON.stringify({
+    command: 'initChannel',
+    balanceA: channelInitState.balanceA.toString(),
+    balanceB: channelInitState.balanceB.toString(),
+    channelId: channelConfig.channelId.toString(),
+    addressB: state.addressB.toString(true, true, true),
+    publicKeyB: TonWeb.utils.bytesToBase64(state.keyPair.publicKey)
+  }))
+  waitForInit()
+}
+
+async function waitForInit() {
+  show(initChannelViewEl)
+}
+
+async function checkInitialized() {
+  const data = await state.channelB.getData()
+  console.log(data)
+  console.log('balanceA: %s', data.balanceA.toString())
+  console.log('balanceB: %s', data.balanceB.toString())
+  x = await state.fromWalletB.init(state.channelInitState).send(toNano('0.05'))
+  console.log('x: %o', x)
+  let i = 30
+  while (i > 0) {
+    i -= 1
+    try {
+      const channelState = await state.channelB.getChannelState()
+      console.log('channel state: %s', channelState)
+      if (channelState !== 0) {
+        break
+      }
+    } catch (e) {
+    }
+    await timeout(1000)
+  }
+  show(hallViewEl)
+  showChannelBalance()
+  hideError()
+}
+
+function setupWebSocket() {
+  const url = "ws://localhost:3000/myWebsocket"
+  const wsServer = new WebSocket(url)
+  state.wsServer = wsServer
+
+  wsServer.onerror = function() {
+    showError('Error connecting to websocket server.')
+  }
+
+  wsServer.onclose = function() {
+    hideAll()
+    showError('Connection to websocket closed.')
+  }
+
+  wsServer.onopen = function() {
+    wsServer.send(JSON.stringify({command: 'getAddress'}))
+  }
+
+  wsServer.onmessage = function(m) {
+    const msg = JSON.parse(m.data)
+    if (msg.event === 'error') {
+      showError(msg.text)
+    } else if (msg.event === 'address') {
+      setupWallet(msg.address, msg.publicKey)
+    } else if (msg.event === 'channelInitialized') {
+      checkInitialized()
+    } else if (msg.event === 'waiting') {
+      show(waitViewEl)
+      hideError()
+    } else if (msg.event === 'left') {
+      gameYourPickEl.innerText = ''
+      gameTheirPickEl.innerText = ''
+      show(hallViewEl)
+    } else if (msg.event === 'joined') {
+      const roomId = msg.roomId
+      gameRoomEl.innerText = roomId
+      show(gameViewEl)
+      clearResult()
+    } else if (msg.event === 'picked') {
+      if (msg.addressB !== state.addressB.toString(true, true, true)) {
+        gameTheirPickEl.innerText = '☑️'
+      }
+    } else if (msg.event === 'draw') {
+      state.channelState = deserializeChannelState(msg.channelState)
+      gameTheirPickEl.innerText = icon(msg.pick)
+      showResult('draw')
+      showChannelBalance()
+      setTimeout(function() {
+        clearResult()
+        enablePickButtons()
+      }, 2000)
+    } else if (msg.event === 'won') {
+      state.channelState = deserializeChannelState(msg.channelState)
+      gameTheirPickEl.innerText = icon(msg.pick)
+      showResult('won')
+      showChannelBalance()
+      setTimeout(function() {
+        clearResult()
+        enablePickButtons()
+      }, 2000)
+    } else if (msg.event === 'lost') {
+      gameTheirPickEl.innerText = icon(msg.pick)
+      showResult('lost')
+      showChannelBalance()
+      setTimeout(function() {
+        clearResult()
+        enablePickButtons()
+      }, 2000)
+    } else {
+      console.log('unknown message: %s', msg)
+    }
+  }
+}
+
+hallJoinEl.addEventListener('click', function() {
+  state.wsServer.send(JSON.stringify({command: 'join'}))
+})
+
+waitLeaveEl.addEventListener('click', function() {
+  state.wsServer.send(JSON.stringify({command: 'leave'}))
+})
+
+gameLeaveEl.addEventListener('click', function() {
+  state.wsServer.send(JSON.stringify({command: 'leave'}))
+})
+
+function getChannelStateForNextRound() {
+  return nextChannelState = {
+    balanceA: state.channelState.balanceA,
+    balanceB: state.channelState.balanceB.sub(toNano('0.1')),
+    seqnoA: state.channelState.seqnoA,
+    seqnoB: state.channelState.seqnoB.add(new BN('1'))
+  }
+}
+
+gamePickREl.addEventListener('click', function() {
+  disablePickButtons()
+  gameYourPickEl.innerText = icon('r')
+  const nextChannelState = getChannelStateForNextRound()
+  state.channelB.signState(nextChannelState).then(function(signatureB) {
+    state.wsServer.send(JSON.stringify({
+      command: 'pick',
+      pick: 'r',
+      nextChannelState: serializeChannelState(nextChannelState),
+      signatureB: serializeSignature(signatureB)
+    }))
+    state.channelState = nextChannelState
+    showChannelBalance()
+  })
+})
+
+gamePickPEl.addEventListener('click', function() {
+  disablePickButtons()
+  gameYourPickEl.innerText = icon('p')
+  const nextChannelState = getChannelStateForNextRound()
+  state.channelB.signState(nextChannelState).then(function(signatureB) {
+    state.wsServer.send(JSON.stringify({
+      command: 'pick',
+      pick: 'p',
+      nextChannelState: serializeChannelState(nextChannelState),
+      signatureB: serializeSignature(signatureB)
+    }))
+    state.channelState = nextChannelState
+    showChannelBalance()
+  })
+})
+
+gamePickSEl.addEventListener('click', function() {
+  disablePickButtons()
+  gameYourPickEl.innerText = icon('s')
+  const nextChannelState = getChannelStateForNextRound()
+  state.channelB.signState(nextChannelState).then(function(signatureB) {
+    state.wsServer.send(JSON.stringify({
+      command: 'pick',
+      pick: 's',
+      nextChannelState: serializeChannelState(nextChannelState),
+      signatureB: serializeSignature(signatureB)
+    }))
+    state.channelState = nextChannelState
+    showChannelBalance()
+  })
+})
+
+function serializeChannelState(channelState) {
+  return {
+    balanceA: channelState.balanceA.toString(),
+    balanceB: channelState.balanceB.toString(),
+    seqnoA: channelState.seqnoA.toString(),
+    seqnoB: channelState.seqnoB.toString()
+  }
+}
+
+function deserializeChannelState(channelState) {
+  return {
+    balanceA: new BN(channelState.balanceA),
+    balanceB: new BN(channelState.balanceB),
+    seqnoA: new BN(channelState.seqnoA),
+    seqnoB: new BN(channelState.seqnoB)
+  }
+}
+
+function serializeSignature(signature) {
+  return TonWeb.utils.bytesToBase64(signature)
+}
+
+function deserializeSignature(signature) {
+  return TonWeb.utils.base64ToBytes(signature)
+}
+
+function icon(x) {
+  if (x === 'r') {
+    return '✊'
+  } else if (x === 'p') {
+    return '🖐'
+  } else if (x === 's') {
+    return '✌️'
+  }
+}
+
+function disablePickButtons() {
+  gamePickREl.disabled = true
+  gamePickPEl.disabled = true
+  gamePickSEl.disabled = true
+}
+
+function enablePickButtons() {
+  gamePickREl.disabled = false
+  gamePickPEl.disabled = false
+  gamePickSEl.disabled = false
+}
+
+function showChannelBalance() {
+  channelBalanceViewEl.style.display = ''
+  channelBalanceEl.innerText = fromNano(state.channelState.balanceB)
+}
+
+function showResult(r) {
+  gameDrawEl.style.display = 'none'
+  gameWonEl.style.display = 'none'
+  gameLostEl.style.display = 'none'
+  if (r === 'draw') {
+    gameDrawEl.style.display = ''
+  } else if (r === 'won') {
+    gameWonEl.style.display = ''
+  } else if (r === 'lost') {
+    gameLostEl.style.display = ''
+  }
+}
+
+function clearResult() {
+  gameDrawEl.style.display = 'none'
+  gameWonEl.style.display = 'none'
+  gameLostEl.style.display = 'none'
+  gameYourPickEl.innerText = ''
+  gameTheirPickEl.innerText = ''
+  enablePickButtons()
+}
---
+++ server.js
@@ -0,0 +1,340 @@
+const path = require('path')
+const WebSocket = require('ws')
+const express = require('express')
+const TonWeb = require('tonweb')
+
+const state = {
+  joinList: [],
+  roomDict: {},
+  nextRoomId: 0,
+  seed: null,
+  keyPair: null,
+  wallet: null,
+  address: null,
+  balance: null
+}
+
+function timeout(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, ms)
+  })
+}
+
+// TonWeb
+const BN = TonWeb.utils.BN
+const toNano = TonWeb.utils.toNano
+const fromNano = TonWeb.utils.fromNano
+const providerUrl = 'https://testnet.toncenter.com/api/v2/jsonRPC'
+const apiKey = '0b673465b3dff8f572d26adb553f2bdabcd894c19d7b74c8104d6b3fb56b00bc'
+const tonweb = new TonWeb(new TonWeb.HttpProvider(providerUrl, {apiKey}))
+setupWallet()
+
+async function setupWallet() {
+  const seedBase64 = 'jGNhby2AUjooi2UCGSKnjnxgLHgSaWNCHxNVkzGVqZA='
+  const seed = TonWeb.utils.base64ToBytes(seedBase64)
+  state.seed = seed
+  state.keyPair = TonWeb.utils.keyPairFromSeed(seed)
+  state.wallet = tonweb.wallet.create({publicKey: state.keyPair.publicKey, wc: 0})
+  state.address = await state.wallet.getAddress()
+  state.balance = await tonweb.getBalance(state.address)
+  console.log('Wallet address: %s', state.address.toString(true, true, true))
+  console.log('Wallet balance: %s TON', fromNano(state.balance))
+}
+
+const app = express()
+app.use('/', express.static(path.resolve(__dirname, './public')))
+const myServer = app.listen(3000, function() {
+  console.log('Listening on http://localhost:3000')
+})
+
+const wsServer = new WebSocket.Server({
+  noServer: true
+})
+
+myServer.on('upgrade', async function upgrade(request, socket, head) {
+  wsServer.handleUpgrade(request, socket, head, function(ws) {
+    wsServer.emit('connection', ws, request)
+  })
+})
+
+wsServer.on('connection', function(ws) {
+  ws.on('message', function(m) {
+    const msg = JSON.parse(m.toString())
+    if (msg.command === 'getAddress') {
+      ws.send(JSON.stringify({
+        event: 'address',
+        address: state.address.toString(true, true, true),
+        publicKey: TonWeb.utils.bytesToBase64(state.keyPair.publicKey)
+      }))
+    } else if (msg.command === 'initChannel') {
+      initChannel(ws, msg)
+    } else if (msg.command === 'join') {
+      queue(ws)
+      ws.send(JSON.stringify({event: 'waiting'}))
+      setTimeout(joinPlayers, 500)
+    } else if (msg.command === 'leave') {
+      leaveRoom(ws)
+    } else if (msg.command === 'pick') {
+      checkPick(ws, msg)
+    } else {
+      console.log('unknown message: %s', msg)
+    }
+  })
+  ws.on('close', function() {
+    leaveRoom(ws)
+  })
+})
+
+async function checkPick(ws, msg) {
+  const nextChannelState = deserializeChannelState(msg.nextChannelState)
+  const signatureB = deserializeSignature(msg.signatureB)
+  if (nextChannelState.balanceA.toString() != ws.channelState.balanceA.toString()) {
+    return console.log('balanceA mismatch')
+  }
+  if (nextChannelState.balanceB.toString() != ws.channelState.balanceB.sub(toNano('0.1')).toString()) {
+    return console.log('balanceB not subtracted by 0.1')
+  }
+  if (nextChannelState.seqnoA.toString() != ws.channelState.seqnoA.toString()) {
+    return console.log('seqnoA mismatch')
+  }
+  if (nextChannelState.seqnoB.toString() != ws.channelState.seqnoB.add(new BN('1')).toString()) {
+    return console.log('seqnoB not added by 1')
+  }
+  if (!(await ws.channelA.verifyState(nextChannelState, signatureB))) {
+    return console.log('signatureB is invalid')
+  }
+  ws.channelState = nextChannelState
+
+  const pick = msg.pick
+  const roomId = ws.roomId
+  const room = state.roomDict[roomId]
+  if (ws === room.playerA) {
+    room.pickA = pick
+  } else if (ws === room.playerB) {
+    room.pickB = pick
+  }
+  const newMsg = JSON.stringify({
+    event: 'picked',
+    addressB: ws.addressB.toString(true, true, true)
+  })
+  room.playerA.send(newMsg)
+  room.playerB.send(newMsg)
+  checkRound(room)
+}
+
+function queue(ws) {
+  state.joinList = state.joinList.filter(function(entry) {
+    if (entry.addressB.toString(true, true, true) !== ws.addressB.toString(true, true, true)) {
+      return true
+    } else {
+      entry.close()
+      return false
+    }
+  })
+  state.joinList.push(ws)
+}
+
+function joinPlayers() {
+  while (state.joinList.length >= 2) {
+    const room = {
+      playerA: state.joinList.pop(),
+      playerB: state.joinList.pop(),
+      pickA: null,
+      pickB: null
+    }
+    state.nextRoomId += 1
+    const roomId = state.nextRoomId
+    room.playerA.roomId = roomId
+    room.playerB.roomId = roomId
+    state.roomDict[roomId] = room
+    const newMsg = JSON.stringify({event: 'joined', roomId: roomId})
+    room.playerA.send(newMsg)
+    room.playerB.send(newMsg)
+  }
+}
+
+function leaveRoom(ws) {
+  state.joinList = state.joinList.filter(function(entry) {
+    return entry !== ws
+  })
+  const newMsg = JSON.stringify({event: 'left'})
+  ws.send(newMsg)
+  const roomId = ws.roomId
+  if (roomId != null) {
+    const room = state.roomDict[roomId]
+    state.roomDict[roomId] = null
+    room.playerA.roomId = null
+    room.playerB.roomId = null
+    if (ws === room.playerA) {
+      room.playerB.send(newMsg)
+    } else {
+      room.playerA.send(newMsg)
+    }
+  }
+}
+
+async function checkRound(room) {
+  const a = room.pickA
+  const b = room.pickB
+  if (a != null && b != null) {
+    await timeout(1000)
+    const result = rpsCheckRule(a, b)
+    if (result === 0) {
+      giveBack(room.playerA)
+      giveBack(room.playerB)
+      const signaturePlayerA = await room.playerA.channelA.signState(room.playerA.channelState)
+      const signaturePlayerB = await room.playerB.channelA.signState(room.playerB.channelState)
+      room.playerA.send(JSON.stringify({
+        event: 'draw',
+        pick: room.pickB,
+        channelState: serializeChannelState(room.playerA.channelState),
+        signatureA: serializeSignature(signaturePlayerA)
+      }))
+      room.playerB.send(JSON.stringify({
+        event: 'draw',
+        pick: room.pickA,
+        channelState: serializeChannelState(room.playerB.channelState),
+        signatureA: serializeSignature(signaturePlayerB)
+      }))
+    } else if (result === 1) {
+      giveBack(room.playerA)
+      giveBack(room.playerA)
+      const signaturePlayerA = await room.playerA.channelA.signState(room.playerA.channelState)
+      room.playerA.send(JSON.stringify({
+        event: 'won',
+        pick: room.pickB,
+        channelState: serializeChannelState(room.playerA.channelState),
+        signatureA: serializeSignature(signaturePlayerA)
+      }))
+      room.playerB.send(JSON.stringify({
+        event: 'lost',
+        pick: room.pickA
+      }))
+    } else if (result === 2) {
+      giveBack(room.playerB)
+      giveBack(room.playerB)
+      const signaturePlayerB = await room.playerB.channelA.signState(room.playerB.channelState)
+      room.playerA.send(JSON.stringify({
+        event: 'lost',
+        pick: room.pickB,
+      }))
+      room.playerB.send(JSON.stringify({
+        event: 'won',
+        pick: room.pickA,
+        channelState: serializeChannelState(room.playerB.channelState),
+        signatureA: serializeSignature(signaturePlayerB)
+      }))
+    }
+    room.pickA = null
+    room.pickB = null
+  }
+}
+
+function giveBack(ws) {
+  console.log(ws.channelState.balanceA)
+  console.log(ws.channelState.balanceB)
+  const nextChannelState = {
+    balanceA: ws.channelState.balanceA.sub(toNano('0.1')),
+    balanceB: ws.channelState.balanceB.add(toNano('0.1')),
+    seqnoA: ws.channelState.seqnoA.add(new BN('1')),
+    seqnoB: ws.channelState.seqnoB
+  }
+  ws.channelState = nextChannelState
+}
+
+function rpsCheckRule(a, b) {
+  if (a === 'r' && b === 'p') {
+    return 2
+  } else if (a === 'p' && b === 's') {
+    return 2
+  } else if (a === 's' && b === 'r') {
+    return 2
+  } else if (a === 'r' && b === 's') {
+    return 1
+  } else if (a === 'p' && b === 'r') {
+    return 1
+  } else if (a === 's' && b === 'p') {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+function initChannel(ws, msg) {
+  const channelInitState = {
+    balanceA: new BN(msg.balanceA),
+    balanceB: new BN(msg.balanceB),
+    seqnoA: new BN(0),
+    seqnoB: new BN(0)
+  }
+  const channelConfig = {
+    channelId: new BN(msg.channelId),
+    addressA: state.address,
+    addressB: new TonWeb.utils.Address(msg.addressB),
+    initBalanceA: channelInitState.balanceA,
+    initBalanceB: channelInitState.balanceB
+  }
+  const channelA = tonweb.payments.createChannel({
+    ...channelConfig,
+    isA: true,
+    myKeyPair: state.keyPair,
+    hisPublicKey: TonWeb.utils.base64ToBytes(msg.publicKeyB)
+  })
+  const fromWalletA = channelA.fromWallet({
+    wallet: state.wallet,
+    secretKey: state.keyPair.secretKey
+  })
+  const checkBalanceA = function() {
+    return channelA.getData().then(function(data) {
+      if (data.balanceA.toString() !== '0') {
+        return
+      } else {
+        return timeout(1000).then(checkBalanceA)
+      }
+    })
+  }
+  fromWalletA
+  .topUp({coinsA: channelInitState.balanceA, coinsB: new BN(0)})
+  .send(channelInitState.balanceA.add(toNano('0.05')))
+  .then(checkBalanceA)
+  .then(function() {
+    return fromWalletA.init(channelInitState).send(toNano('0.05'))
+  })
+  .then(function() {
+    ws.channelState = channelInitState
+    ws.addressB = channelConfig.addressB
+    ws.channelA = channelA
+    ws.fromWalletA = fromWalletA
+    ws.send(JSON.stringify({event: 'channelInitialized'}))
+  })
+  .catch(function(e) {
+    console.log('error in initializing channel: %s', e)
+    ws.send(JSON.stringify({event: 'error', text: 'error in initializing channel'}))
+  })
+}
+
+function serializeChannelState(channelState) {
+  return {
+    balanceA: channelState.balanceA.toString(),
+    balanceB: channelState.balanceB.toString(),
+    seqnoA: channelState.seqnoA.toString(),
+    seqnoB: channelState.seqnoB.toString()
+  }
+}
+
+function deserializeChannelState(channelState) {
+  return {
+    balanceA: new BN(channelState.balanceA),
+    balanceB: new BN(channelState.balanceB),
+    seqnoA: new BN(channelState.seqnoA),
+    seqnoB: new BN(channelState.seqnoB)
+  }
+}
+
+function serializeSignature(signature) {
+  return TonWeb.utils.bytesToBase64(signature)
+}
+
+function deserializeSignature(signature) {
+  return TonWeb.utils.base64ToBytes(signature)
+}
